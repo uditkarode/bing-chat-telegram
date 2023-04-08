5fdda37f60fd4aae2e348b06b6139c746ee57628
Udit Karode <udit.karode@gmail.com>
2023-04-08 11:18:47 +0530

    doc(variables.example.ts): we also remove double quotes

--- variables.example.ts
+++ variables.example.ts
@@ -10,7 +10,7 @@ export const ALLOWED_CHAT_IDS: string | number[] = (() => {
 // Enter 'document.cookie' in the console on bing.com,
 // and paste the result here. You must be logged in to
 // your Microsoft account and have access to Bing Chat.
-// PS: don't worry about the newlines, they will be trimmed
+// PS: don't worry about the newlines or double quotes (" "), they will be trimmed
 export const BING_COOKIE = `
 
 `;
